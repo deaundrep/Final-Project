b8467e0b1be446516abf7bf3e4fd6ffa14c28517
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-04-29 12:59:08 -0400

    rater

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
 function myFunction() {
     var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-    x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
     x.style.display = "none";
+    } else {
+    x.style.display = "block";
     }
 }
 
 function myFunction1() {
     var x = document.getElementById("myDIV1");
-    if (x.style.display === "none") {
-    x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
     x.style.display = "none";
+    } else {
+    x.style.display = "block";
     }
 }
 
 function myFunction2() {
     var x = document.getElementById("myDIV2");
-    if (x.style.display === "none") {
-    x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
     x.style.display = "none";
+    } else {
+    x.style.display = "block";
     }
 }
+
+function myFunction3() {
+    var x = document.getElementById("myDIV3");
+    if (x.style.display === "block") {
+    x.style.display = "none";
+    } else {
+    x.style.display = "block";
+    }
+}
+
+
+
+
+
+    
 
